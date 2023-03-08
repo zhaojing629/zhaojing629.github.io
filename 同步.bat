47ebb46ac59ca2c07f916a2b5248c02df3273f5a
zhaojing629 <295483858@qq.com>
2023-03-08 16:41:07 +0800

    111

--- 同步.bat
+++ 同步.bat
@@ -1,8 +1,8 @@
 cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
 ren .deploy_git\.git1 .git
-
+hexo g -d
 ren .deploy_git\.git .git1
 git add .
-git commit --no-verify –m "更新" 
+git commit 
 git push origin source
-cmd \k
+pause
