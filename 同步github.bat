8c2a544e039dd48ff7243eb1a09d3291e5a2094a
zhaojing629 <295483858@qq.com>
2023-03-08 17:10:40 +0800

    20230308

--- 同步github.bat
+++ 同步github.bat
@@ -1,8 +1,10 @@
+
+set MESSEGE=%date:~0,4%%date:~5,2%%date:~8,2%
+
 cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
 ren .deploy_git\.git .git1
 git add .
-git commit -m"3"
+git commit -m"%MESSEGE%"
 git push origin source
-set MESSEGE=%date:~0,4%%date:~5,2%%date:~8,2%
-mkdir %MESSEGE%
+
 cmd /k
