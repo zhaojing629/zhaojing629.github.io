61aefa7b19bafaaf971317cb50504bb9201b3252
zhaojing629 <295483858@qq.com>
2023-03-08 17:12:00 +0800

    updated: 20230308

--- 同步github.bat
+++ 同步github.bat
@@ -4,6 +4,6 @@ set MESSEGE=%date:~0,4%%date:~5,2%%date:~8,2%
 cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
 ren .deploy_git\.git .git1
 git add .
-git commit -m"更新于%MESSEGE%"
+git commit -m"updated: %MESSEGE%"
 git push origin source
 cmd /k
