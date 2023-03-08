d66e27a53b884dc090e71a4f9ee95652f26a0482
zhaojing629 <295483858@qq.com>
2023-03-08 17:20:00 +0800

    updated: 2023-03-08 17:19:59

--- 同步github.bat
+++ 同步github.bat
@@ -1,7 +1,8 @@
-
 set riqi=%date:~0,4%-%date:~5,2%-%date:~8,2%
 set shijian=%time:~0,2%:%time:~3,2%:%time:~6,2%
+
 cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
+
 ren .deploy_git\.git .git1
 git add .
 git commit -m"updated: %riqi% %shijian%"
