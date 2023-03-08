1c559089379f0ec7c64bae7b01cd5c2f359d3f08
zhaojing629 <295483858@qq.com>
2023-03-08 16:57:35 +0800

    3

---
+++ 同步github.bat
@@ -0,0 +1,6 @@
+cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
+ren .deploy_git\.git .git1
+git add .
+git commit -m"3"
+git push origin source
+pause
--- 同步网页.bat
+++ 同步网页.bat
@@ -1,8 +1,4 @@
 cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
 ren .deploy_git\.git1 .git
 hexo g -d
-ren .deploy_git\.git .git1
-git add .
-git commit -m"更新"
-git push origin source
 pause
