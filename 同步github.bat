bfbb4c77fb01f6095bbeb0f5233fda0d58f1c438
zhaojing629 <295483858@qq.com>
2023-03-08 17:15:53 +0800

    updated: 17:15:53

--- 同步github.bat
+++ 同步github.bat
@@ -1,9 +1,9 @@
 
-set MESSEGE=%date:~0,4%-%date:~5,2%-%date:~8,2%
-
+set riqi=%date:~0,4%-%date:~5,2%-%date:~8,2%
+set shijian=%time:~0,2%:%time:~3,2%:%time:~6,2%
 cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
 ren .deploy_git\.git .git1
 git add .
-git commit -m"updated: %MESSEGE%"
+git commit -m"updated: %shijian%"
 git push origin source
 cmd /k
