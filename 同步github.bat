491df3c5fead8900ca1d77694c8dd0d92e90e3f0
zhaojing629 <295483858@qq.com>
2023-03-08 17:13:05 +0800

    updated: 2023-03-08

--- 同步github.bat
+++ 同步github.bat
@@ -1,5 +1,5 @@
 
-set MESSEGE=%date:~0,4%%date:~5,2%%date:~8,2%
+set MESSEGE=%date:~0,4%-%date:~5,2%-%date:~8,2%
 
 cd "D:\10_Hexo_blog(502a+thuyun)\zhaojing629.github.io"
 ren .deploy_git\.git .git1
